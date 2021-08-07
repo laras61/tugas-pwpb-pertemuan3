976ee8867c6d01d05f20b9903b0bee04c819fd33
larasmey <69838304+laras61@users.noreply.github.com>
2021-08-07 19:05:54 +0700

    Add files via upload

---
+++ tugas1.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pertemuan 3 Tugas 1 Laras Melati</title>
+</head>
+<body>
+    <table border="1" width="500px" height="600px" align="center">
+
+        <!-- Banner -->
+        <tr height="50px">
+            <td>
+
+            </td>
+        </tr>
+
+        <!-- Main -->
+        <tr height="400px">
+            <td>
+                <table border="1" width="100%" height="100%">
+                    <tr height=" 130px" >
+                        <td>
+                            <table border="1" width="80%" height="80%" align="center">
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td colspan="2"></td>
+                                    <td colspan="3"></td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                    <tr height="270px" >
+                        <td>
+                            <table border="1" width="80%" height="80%" align="center">
+                                <tr height="20%">
+                                    <td colspan="2"></td>
+                                </tr>
+                                <tr height="80%">
+                                    <td width="50%"></td>
+                                    <td>
+                                        <table border="1" width="80%" height="80%" align="center">
+                                            <tr height="10%">
+                                                <td></td>
+                                            </tr>
+                                            <tr height="10%">
+                                                <td></td>
+                                            </tr>
+                                            <tr height="30%">
+                                                <td></td>
+                                            </tr>
+                                            <tr height="10%">
+                                                <td></td>
+                                            </tr>
+                                            <tr height="10%">
+                                                <td></td>
+                                            </tr>
+                                            <tr height="30%">
+                                                <td></td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+
+        <!-- Footer -->
+        <tr height="150px">
+            <td>
+                <table border="1" width="100%" height="100%" align="center">
+                    <tr height="50%">
+                        <td>
+                            <table border="1" width="40%" height="95%" align="center">
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                                <tr height="20%">
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                    <tr height="50%">
+                        <td>
+                            <table border="1" width="50%" height="95%" align="center">
+                                <tr height="60%">
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td colspan="4"></td>
+                                    <td></td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
---
+++ tugas2.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pertemuan 3 tugas 2 Laras Melati</title>
+</head>
+<body bgcolor="bisque">
+    <h1 align="center">Pertemuan 3</h1>
+    <h2 align="center">Tugas ke 2</h2>
+    <p align="center">Laras Melati XI RPL 2</p>
+    <center>Pemrograman Web dan Perangkat Bergerak</center>
+    <a href="https://facebook.com">facebook</a>
+    <a href="https://instagram.com">instagram</a>
+    <hr>
+    <table  cellspacing="0" cellpadding="2" border="1" width="200px" height="100px" align="center">
+        <tr>
+            <th>ini tabel</th>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td rowspan="2"></td>
+        </tr>
+        <tr>
+            <td colspan="2"></td>
+        </tr>    
+    </table>
+    <pre>Berikut adalah daftar gambar </pre>
+    <ol>
+        <li>Gambar kucing</li>
+    </ol>
+    <ul>
+        <li>deskripsi kucing</li>
+    </ul>
+    <dl>
+        <dt>KUCING</dt>
+        <dd>kucing adalah makhluk hidup ciptaan tuhan. ia bisa bernapas, mempunyai tingkah laku, merasakan lapar dan haus. kita sebagai manusia jangan pernah menyakiti kucing.</dd>
+    </dl>
+    <b>foto kucing dari google</b>
+    <img src="https://tse1.mm.bing.net/th?id=OIP.xxE1iDPhCfhQ5ZYE0kHZrwHaE6&pid=Api&P=0&w=226&h=151" >
+    <font face="arial" size="12"></font>
+    <div style="width:200px;height:100px;"></div>
+    <hr noshade>
+    <p align="center">punya @laras</p>
+</body>
+</html>
